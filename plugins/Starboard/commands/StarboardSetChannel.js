62b34ff893e9b150d31f08ab7044b1584bd29990
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-21 23:52:34 -0400

    Fix embeds // Change comamnd name

--- plugins/Starboard/commands/StarboardSetChannel.js
+++ plugins/Starboard/commands/StarboardSetChannel.js
@@ -5,14 +5,23 @@ const { PermissionFlagsBits } = require("discord.js");
 module.exports = class extends Command {
   constructor(client) {
     super(client, {
-      name: "setstarboardchannel",
+      name: "starboardsetchannel",
       enabled: true,
-      syntax: "setstarboardchannel <#channel>",
       staffOnly: true,
+      aliases: ['ssch', 'starsetch'],
+      syntax: "ssch <channel>",
+      about: 'Set the channel for Starboard',
+      example: 'starsetch #board',
     });
   }
   async execute(message, args) {
     const channel = message.mentions.channels.first();
+
+    if (args.length === 0) {
+      return message.reply({
+        embeds: [new WrongSyntaxEmbed(this.client, message, this)],
+      });
+    }
 
     if (!channel)
       return message.reply({
@@ -23,7 +32,7 @@ module.exports = class extends Command {
 
     const embed = new SuccessEmbed({
       description: `Successfully set ${channel} for starboard.`,
-    });
+    },message);
 
     await message.reply({ embeds: [embed] });
   }
