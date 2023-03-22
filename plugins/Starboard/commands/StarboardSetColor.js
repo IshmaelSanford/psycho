6fc9ac865e1a579b09676a42a4de6d61d80e7194
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-21 23:53:11 -0400

    Disable command

--- plugins/Starboard/commands/StarboardSetColor.js
+++ plugins/Starboard/commands/StarboardSetColor.js
@@ -5,13 +5,23 @@ const { PermissionFlagsBits, resolveColor } = require("discord.js");
 module.exports = class extends Command {
   constructor(client) {
     super(client, {
-      name: "starboard-set-color",
-      enabled: true,
-      syntax: "starboard-set-color <hex>",
+      name: "starboardsetcolor",
+      enabled: false,
       staffOnly: true,
+      aliases: ['sscl', 'starsetcl'],
+      syntax: "ssch <hex>",
+      about: 'Set the color for Starboard',
+      example: 'starsetcl #000000',
     });
   }
   async execute(message, args) {
+    
+    if (args.length === 0) {
+      return message.reply({
+        embeds: [new WrongSyntaxEmbed(this.client, message, this)],
+      });
+    }
+
     try {
       resolveColor(args[0]);
     } catch {
@@ -26,7 +36,7 @@ module.exports = class extends Command {
 
     const embed = new SuccessEmbed({
       description: `Successfully set color for starboard.`,
-    });
+    },message);
 
     await message.reply({ embeds: [embed] });
   }
