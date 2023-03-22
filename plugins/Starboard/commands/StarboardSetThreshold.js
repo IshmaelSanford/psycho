da6f6359b5d0f6c3375ad25367c676272c666add
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-21 23:54:21 -0400

    Fix embeds

--- plugins/Starboard/commands/StarboardSetThreshold.js
+++ plugins/Starboard/commands/StarboardSetThreshold.js
@@ -9,13 +9,22 @@ const { PermissionFlagsBits } = require("discord.js");
 module.exports = class extends Command {
   constructor(client) {
     super(client, {
-      name: "starboard-set-threshold",
+      name: "starboardsetthreshold",
       enabled: true,
-      syntax: "starboard-set-threshold <amount>",
       staffOnly: true,
+      aliases: ['sst', 'starsetth'],
+      syntax: "ssch <ammount>",
+      about: 'Set the threshold for Starboard',
+      example: 'starsetch 3',
     });
   }
   async execute(message, args) {
+    if (args.length === 0) {
+      return message.reply({
+        embeds: [new WrongSyntaxEmbed(this.client, message, this)],
+      });
+    }
+    
     const amount = parseInt(args[0]);
 
     if (isNaN(amount))
@@ -27,7 +36,7 @@ module.exports = class extends Command {
 
     const embed = new SuccessEmbed({
       description: `Successfully set \`${amount}\` for starboard threshold.`,
-    });
+    },message);
 
     await message.reply({ embeds: [embed] });
   }
--- plugins/Starboard/commands/StarboardToggle.js
+++ plugins/Starboard/commands/StarboardToggle.js
@@ -5,13 +5,22 @@ const { PermissionFlagsBits, messageLink } = require("discord.js");
 module.exports = class extends Command {
   constructor(client) {
     super(client, {
-      name: "starboard-toggle",
+      name: "starboardtoggle",
       enabled: true,
-      syntax: "starboard-toggle",
       staffOnly: true,
+      aliases: ['st', 'startoggle'],
+      syntax: "st",
+      about: 'Toggle Starboard',
+      example: 'startoggle',
     });
   }
   async execute(message, args) {
+    if (args.length === 0) {
+      return message.reply({
+        embeds: [new WrongSyntaxEmbed(this.client, message, this)],
+      });
+    }
+    
     let status = this.client.plugins.starboard.getStatus(message.guild);
 
     await this.client.plugins.starboard.toggle(message.guild);
@@ -20,7 +29,7 @@ module.exports = class extends Command {
       description: `Successfully toggled starboard to \`${
         !status === true ? "enabled" : "disabled"
       }\``,
-    });
+    },message);
 
     await message.reply({ embeds: [embed] });
   }
