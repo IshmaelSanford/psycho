7b2a438bae16bce890908a425777d8c23ca36273
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-21 23:37:00 -0400

    Add static color function // Update embed changes

--- plugins/Boost/commands/Br-Hex.js
+++ plugins/Boost/commands/Br-Hex.js
@@ -3,8 +3,10 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const {
   DefaultEmbed,
   WrongSyntaxEmbed,
+  ErrorEmbed,
   SuccessEmbed,
 } = require("../../../embeds");
+const colorNamer = require("color-namer");
 
 const { resolveColor } = require("discord.js");
 
@@ -12,29 +14,50 @@ module.exports = class extends Command {
   constructor(client) {
     super(client, {
       name: "br-hex",
-      enabled: true,
+      enabled: false,
     });
   }
+
+  static isValidColor(color) {
+    return /^#(?:[0-9a-fA-F]{3}){1,2}$/.test(color);
+  }
+
   async execute(message, args) {
+    if (args.length === 0) {
+      return message.reply({
+        embeds: [new WrongSyntaxEmbed(this.client, message, this)],
+      });
+    }
+  
     let role = this.client.plugins.boost.getRole(message.member);
     role = message.guild.roles.cache.get(role);
-
+  
     if (!role)
       return message.reply({ content: "❌ You don't have booster role!" });
-
-    const color = resolveColor(args[0]);
-
-    if (!color)
+  
+    const inputColor = args[0];
+  
+    if (!this.constructor.isValidColor(inputColor)) {
       return message.reply({
-        embeds: [new WrongSyntaxEmbed(this.name, this.syntax)],
+        embeds: [
+          new ErrorEmbed({
+            description: `${message.author.toString()}: Wrong notation. Try adding **#** or go [here](https://htmlcolorcodes.com/color-picker/) for help`,
+          }),
+        ],
       });
+    }
+  
+    const color = resolveColor(inputColor);
+  
+    role = await role.edit({ color: color });
 
-    role.edit({ color: color });
+    const colorName = colorNamer(role.hexColor).html[0].name;
 
     const embed = new SuccessEmbed({
-      description: `Successfully set color \`${color}\` for your booster role.`,
+      description: `${message.author.toString()}: Set color **${colorName}** (\`${role.hexColor}\`) to your booster role`,
     });
-
+  
     await message.reply({ embeds: [embed] });
   }
+  
 };
