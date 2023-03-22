360d408964f87e9de721142c7b5647c290752356
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-21 23:28:01 -0400

    Include message author in description

--- embeds/ErrorEmbed.js
+++ embeds/ErrorEmbed.js
@@ -1,10 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
 
 class ErrorEmbed extends EmbedBuilder {
-  constructor(data) {
+  constructor(data, message) {
     super(data);
     this.data.color = 0xf4686a;
-    this.data.description = `<:deny:1086430263489351761> ${data.description}`;
+    this.data.description = `<:deny:1086430263489351761> ${message.author.toString()}: ${data.description}`;
   }
 }
 
--- embeds/SuccessEmbed.js
+++ embeds/SuccessEmbed.js
@@ -1,10 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
 
 class SuccessEmbed extends EmbedBuilder {
-  constructor(data) {
+  constructor(data, message) {
     super(data);
     this.data.color = 0xacec7c;
-    this.data.description = `<:approve:1086430347220221992> ${data.description}`;
+    this.data.description = `<:approve:1086430347220221992> ${message.author.toString()}: ${data.description}`;
   }
 }
 
