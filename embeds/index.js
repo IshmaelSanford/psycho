08a171287b6565406160d891f5d2a2881430d2e4
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-28 18:04:51 -0400

    Added WarnEmbedDm

--- embeds/index.js
+++ embeds/index.js
@@ -4,4 +4,5 @@ module.exports = {
   SuccessEmbed: require("./SuccessEmbed"),
   WrongSyntaxEmbed: require("./WrongSyntaxEmbed"),
   WarnEmbed: require('./WarnEmbed'),
+  WarnEmbedDm: require('./WarnEmbedDm'),
 };
