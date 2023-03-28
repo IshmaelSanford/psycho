8bfeafbf6b53ae32ce00cf0f4d52d3d6835246dc
ish <89823985+IshmaelSanford@users.noreply.github.com>
2023-03-28 18:12:13 -0400

    misc changes

--- plugins/Misc/commands/MemberCount.js
+++ plugins/Misc/commands/MemberCount.js
@@ -6,18 +6,36 @@ module.exports = class extends Command {
     super(client, {
       name: "membercount",
       enabled: true,
-      aliases: ['mcount', 'members'],
+      aliases: ['mc', 'members'],
       syntax: "mcount",
       about: 'See how many members the server has',
       example: 'membercount',
     });
   }
   async execute(message) {
+    const user = message.mentions.users.first() || message.author;
+    const member = message.guild.members.cache.get(user.id);
     let bots = message.guild.members.cache.filter((x) => x.user.bot).size;
+    let users = message.guild.memberCount - bots
+
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    const membersJoinedPastWeek = message.guild.members.cache.filter(
+      (m) => m.joinedTimestamp >= oneWeekAgo.getTime()
+    ).size;
+
 
     const embed = new DefaultEmbed()
-      .setDescription(`👥 There are **${message.guild.memberCount - bots} members** in this server`)
-      .setColor('#226699');
+      .setAuthor({
+        name: `${message.guild.name} statistics`,
+        iconURL: this.client.user.displayAvatarURL({ dynamic: true }),
+      })
+      .addFields(
+        { name: `Users (+${membersJoinedPastWeek})`, value: `${users}`, inline: true },
+        { name: "Humans", value: `${message.guild.memberCount}`, inline: true },
+        { name: "Bots", value: `${bots}`, inline: true }
+      )
+      .setColor(member.displayColor === 0 ? "#23272A" : member.displayColor);
     await message.channel.send({ embeds: [embed] });
   }
 };
